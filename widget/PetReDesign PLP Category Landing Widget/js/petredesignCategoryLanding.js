617e41b22ddf44bdf49eef1aae195ddda41c8ec0
Thanigaivelan Chandrasekar <tchandrasekar@petmate.com>
2021-03-26 16:34:33 -0500

    FIx for hard coded brand navigation

--- widget/PetReDesign PLP Category Landing Widget/js/petredesignCategoryLanding.js
+++ widget/PetReDesign PLP Category Landing Widget/js/petredesignCategoryLanding.js
@@ -34,28 +34,28 @@ define(
 		          if(widget.category().fixedParentCategories[i].route.indexOf('cat')!='-1'){
 		              widget.koDisplayCategoryURL([]);
 		                widget.koDisplayCategoryURL.push({
-		                         "route":'/cats/category/cats',
+		                         "route":'/category/cats',
 		                         "displayName":"Cats"  
 		                      });
 		          }
 		           if(widget.category().fixedParentCategories[i].route.indexOf('bird')!='-1'){
 		                widget.koDisplayCategoryURL([]);
 		               widget.koDisplayCategoryURL.push({
-		                         "route":'/birds-small-pets/category/birds',
+		                         "route":'/category/birds',
 		                         "displayName":"Birds & small pets"  
 		                      });
 		          }
 		           if(widget.category().fixedParentCategories[i].route.indexOf('dog')!='-1'){
 		                widget.koDisplayCategoryURL([]);
 		                widget.koDisplayCategoryURL.push({
-		                         "route":'/dogs/category/dogs',
+		                         "route":'/category/dogs',
 		                         "displayName":"Dogs"  
 		                      });
 		          }
 		           if(widget.category().fixedParentCategories[i].route.indexOf('petmate-brands')!='-1'){
 		                widget.koDisplayCategoryURL([]);
 		                widget.koDisplayCategoryURL.push({
-		                         "route":'/brands/category/petmate-brands',
+		                         "route":'/category/petmate-brands',
 		                         "displayName":"Brands"  
 		                      });
 		          }
