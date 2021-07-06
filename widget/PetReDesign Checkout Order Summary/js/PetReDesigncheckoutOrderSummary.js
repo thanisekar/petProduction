cd1cb79253e1bffb5c85843abde6c49cfb592565
Thanigaivelan Chandrasekar <tchandrasekar@petmate.com>
2021-07-06 12:16:10 -0500

    Shown the promo text

--- widget/PetReDesign Checkout Order Summary/js/PetReDesigncheckoutOrderSummary.js
+++ widget/PetReDesign Checkout Order Summary/js/PetReDesigncheckoutOrderSummary.js
@@ -116,7 +116,7 @@ define(
                         $(".CC-checkoutOrderSummary-shippingOption-300008").insertAfter(".CC-checkoutOrderSummary-shippingOption-300009");
                         $("#CC-checkoutOrderSummary-shippingOptionPrice-300010").text("Ground (Standard Shipping: 3 - 5 Business Days)");
                         $("#CC-checkoutOrderSummary-shippingOptionPrice-300009").text("Two Day Shipping: Expect Within 2 Business Days");
-                        $( "#CC-checkoutOrderSummary-discounts span:contains('FREE Ground Shipping All Orders')" ).css( "display", "none" );
+                        //$( "#CC-checkoutOrderSummary-discounts span:contains('FREE Ground Shipping All Orders')" ).css( "display", "none" );
                         },3500)
                 });
                 //Popup
